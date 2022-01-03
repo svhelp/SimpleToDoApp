0dae29c62e161d1fc9ffd0a84ca840bd01daf082
svhelp <svhelpp@gmail.com>
2022-01-03 13:25:10 +0200

    - clean up;

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import { useAppDispatch } from './App/hooks';
 import { initialFetchAsync } from './Features/TodoList/thunks';
 import { TodoList } from './Features/TodoList/Components/TodoList';
--- UI/src/Features/TodoList/thunks.ts
+++ UI/src/Features/TodoList/thunks.ts
@@ -3,11 +3,6 @@ import * as api from "./todoListApi";
 import { IUpdateItemPayload } from "./todoListApi";
 import { addItem, removeItem, updateItem } from "./todoListSlice";
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
 export const initialFetchAsync = createAsyncThunk(
     'todoList/initialFetch',
     async () => {
